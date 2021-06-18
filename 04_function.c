81cf462b08c2524ab0bc9e07eeb23f0b30c222a6
kawaegle <alecromski>
2021-06-18 16:29:00 +0200

    use a function to read and print input

---
+++ 04_function.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+//#include <string.h>
+
+void hello_world(char *word);
+
+void hello_world(char *word){
+	printf("%s", word);
+}
+
+int main(int argc, char const *argv[]){
+	char  str[128];
+	fgets(str, sizeof(str), stdin);
+	hello_world(str);
+	return 0;
+}
